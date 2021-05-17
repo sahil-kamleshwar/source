17469a27bb0de3b38ce412f64e4556f82c07e352
sahil-kamleshwar <77151429+sahil-kamleshwar@users.noreply.github.com>
2021-05-17 20:22:10 +0530

    Add files via upload

---
+++ object.js
@@ -0,0 +1,23 @@
+/* let car= {
+    make: 'bmw',
+    model: '720d',
+    year: '2019',
+    getPrice: function() {
+    return 500;
+} , 
+printDescription:function() {
+    console.log(this.make + ' ' + this.model);
+}
+}
+car.printdescrption ();
+console.log(car.year); */
+
+var c={
+    myProperty : [
+        {d:'this'},
+        {e:'can'},
+        {f:'get'},
+        {g:'crazy'},
+    ]
+};
+console.log(myProperty.c);
